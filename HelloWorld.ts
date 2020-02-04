046e3f02ccd28964d154fa6c6e6f602be30b04cd
Xayaphone <xaysalinthone@gmail.com>
2020-02-03 17:09:36 -0800

    printing greetings

--- HelloWorld.ts
+++ HelloWorld.ts
@@ -1,11 +1,13 @@
 class HelloWorld {
 
     constructor(firstName:string, lastName: string){
-
+        this.firstName = firstName;
     }
-    greet(){
+    greeting(){
         console.log('Hello World!'+ 'My name is ' + this.firstName + ' ' + this.lastName); 
         
     }
     
 }
+const classInstance = new HelloWorld('Xayaphone', 'Salinthone');
+classInstance.greeting();
